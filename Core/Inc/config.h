3a1c283248661bcbf18f862f2234b9ac86eee8cd
aftwro <trollingmaster1231@gmail.com>
2022-12-21 21:38:10 +0300

    Configuration for Ximi 3, No field weakening
    Comparison with Field weakening and no field weakening

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -1,21 +1,21 @@
 #define THROTTLEOFFSET 45
-#define THROTTLEMAX 175
+#define THROTTLEMAX 195
 #define BRAKEOFFSET 50
 #define BRAKEMAX 100
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_ECO 11
+#define SPEEDLIMIT_NORMAL 22
+#define SPEEDLIMIT_SPORT 36
 
 // motor current limits for invividual modes in mA
 // note that hacked firmware allows up to 55amps motor phase current
-#define PH_CURRENT_MAX_ECO 10000
-#define PH_CURRENT_MAX_NORMAL 15000
-#define PH_CURRENT_MAX_SPORT 25000
+#define PH_CURRENT_MAX_ECO 14000
+#define PH_CURRENT_MAX_NORMAL 18000
+#define PH_CURRENT_MAX_SPORT 30000
 
 // motor current limit for regen in mA
-#define REGEN_MAX_CURRENT 10000
+#define REGEN_MAX_CURRENT 0
 
 // maximum current for field weakening in mA
 #define FIELD_WEAKNING_CURRENT_MAX 0 //max id
